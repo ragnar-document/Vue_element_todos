9b733b3eb00efeb83d2948d2e2240ce94541f4eb
董兴龙 <ragnar@dongxinglongdeMacBook-Pro.local>
2019-04-27 19:32:04 +0800

    正在施工

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,28 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <router-link to="/">Home</router-link>
+    <router-view></router-view>
   </div>
 </template>
 
 <style lang="scss">
+*{
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+html{
+  height: 100%;
+  background: #4997e4;
+}
+a{
+  text-decoration: none;
+  color: #fff;
+
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,142 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>vue_ele_todos</h1>
+    <el-container>
+      <el-header>
+        <el-input
+          placeholder="请输入内容"
+          @change="addTodos()"
+          v-model="input"
+          clearable
+          >
+        </el-input>
+      </el-header>
+      <el-main>
+        <ul>
+          <li v-bind:class="[item.judge ? 'active' : '', 'addline']" v-for="item in todos" :key="item.id" >
+            <input class="el-option" type="checkbox"  v-model="item.judge"  @click="switchover(item)">
+            {{item.text}}
+            <p class="remove" @click="removeOnce(item)">X</p>
+          </li>
+        </ul>
+      </el-main>
+      <el-footer>
+        <input class="allCheckbox" type="checkbox">
+        <ul>
+          <!-- <li><a href="/">全部</a></li>
+          <li><a href="/">待办/a></li>
+          <li><a href="/">已办</a></li> -->
+        </ul>
+      </el-footer>
+    </el-container>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      input: '',
+      radio: '',
+      todos:[{
+        id: 1,
+        text: '测试一',
+        judge: false,
+      },{
+        id: 2,
+        text: '测试二',
+        judge: true
+      }]
+    }
   },
-};
+  methods:{
+    addTodos:function(){
+      let value = this.input
+      if (!value) {
+        return
+      }
+      this.todos.push({id:this.todos.length + 1,text:value})
+      this.input = ''
+    },
+    removeOnce:function (item) {
+      let index =this.todos.indexOf(item)
+      this.todos.splice(index,1)
+    },
+    switchover:function(item){
+      let judge = item.judge;
+      judge =  !judge;
+    }
+  }
+}
 </script>
+
+<style>
+h1{
+  color: #fff;
+}
+li{
+  list-style: none
+}
+.el-container{
+  margin: 0 auto;
+  width: 500px;;
+  background: #fff;
+  border-radius: 20px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, .2), 0 0 6px rgba(0, 0, 0, .04)
+}
+.el-container .el-header{
+  position: relative;
+  border-bottom: 1px solid rgba(0, 0, 0, .2);
+}
+.el-container .el-main{
+  border-bottom: 1px solid rgba(0, 0, 0, .2);
+}
+.el-input--suffix{
+  height: 100%;
+  line-height: 4;
+}
+.el-container .el-header .el-input__inner{
+  border: none
+}
+.el-option{
+  position: absolute;
+  top: 5px;
+  left: 5px;
+  float: left;
+  height: 20px;
+  width: 20px;
+}
+.addline{
+  position: relative;
+  line-height: 30px;
+  border-bottom: 1px solid #4997e4;
+  margin-bottom: 10px;
+  padding-left: 30px;
+  text-align: left;
+}
+.addline.active{
+  background: #4997e4;
+  color: #fff;
+  border-top-right-radius: 8px;
+  border-top-left-radius: 8px;
+  transition: all ease 0.2s;
+}
+.addline:hover .remove{
+  color: #4997e4;
+}
+
+.remove{
+  float: right;
+  padding: 5px;
+  width: 10px;
+  height: 10px;
+  margin: auto 0;
+  font-size: 16px;
+  color: #fff;
+  transition: color 0.2s ease-out;
+  cursor: pointer;
+}
+.allCheckbox{
+
+}
+</style>
